ad6b858e22ea5f425b9a254a9a2878cd31c2c9e3
luizidebook <47362223+luizidebook@users.noreply.github.com>
2026-08-06 13:11:10 -0300

    feat(runtime): export public runtime API

---
+++ packages/platform-runtime/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  createPlatformRuntime,
+  EventBus,
+  ModuleRegistry,
+  type DestinationRuntimeConfig,
+  type EventHandler,
+  type PlatformEvent,
+  type PlatformModule,
+  type PlatformRuntime,
+} from "./runtime.js";
