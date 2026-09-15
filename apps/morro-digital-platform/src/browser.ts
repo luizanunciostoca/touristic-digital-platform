78931eed5c99f8aa37b7c93947cd58993b909144
Idebook Digital <luizanunciostoca@gmail.com>
2026-09-15 15:30:54 -0300

    fix(map): render V1 explore POI marker elements

--- apps/morro-digital-platform/src/browser.ts
+++ apps/morro-digital-platform/src/browser.ts
@@ -12,6 +12,7 @@ import {
   loadMorroMapboxRuntimeConfig,
   type RuntimeEnvironment,
 } from "./config/mapbox-runtime.js";
+import { createV1ExploreMarkerElement } from "./map/explore-marker-element.js";
 
 export interface BrowserMapContainer {
   setAttribute(name: string, value: string): void;
@@ -52,9 +53,9 @@ export async function startMorroDigitalBrowser(
       initializeGeospatial: createMorroGeospatialInitializer({
         sdk: options.sdk,
         ...config,
-        ...(options.createMarkerElement
-          ? { createMarkerElement: options.createMarkerElement }
-          : {}),
+        createMarkerElement: (input) =>
+          options.createMarkerElement?.(input) ??
+          createV1ExploreMarkerElement(input),
         ...(options.onMapCreated ? { onMapCreated: options.onMapCreated } : {}),
       }),
       ...(options.initialMarkers
