795bcd550184ac4b11314da9a3572072c1811686
Idebook Digital <luizanunciostoca@gmail.com>
2026-09-24 00:21:44 -0300

    style: format navigation guidance update

--- apps/morro-digital-platform/src/navigation/navigation-guidance-ui.ts
+++ apps/morro-digital-platform/src/navigation/navigation-guidance-ui.ts
@@ -464,11 +464,7 @@ export function createNavigationGuidanceUi(
       destroyed = true;
       minimizeButton?.removeEventListener("click", toggleMinimized);
       supportUi.recenter?.removeEventListener("click", requestRecenter);
-      voiceButton?.removeEventListener(
-        "click",
-        onNavigationStopVoiceClick,
-        true,
-      );
+      voiceButton?.removeEventListener("click", onNavigationStopVoiceClick, true);
     },
   });
 }
